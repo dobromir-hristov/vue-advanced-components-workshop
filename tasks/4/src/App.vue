784b305e6420be2601f5bf7e225ccba383b8b3d4
Dobromir Hristov <dobromir92@gmail.com>
2019-04-07 13:14:19 +0300

    feat (task4): extract modal component

--- tasks/4/src/App.vue
+++ tasks/4/src/App.vue
@@ -1,39 +1,47 @@
 <template>
   <div id="app" class="container mt-4">
-    <button class="btn btn-primary">
+    <button
+      class="btn btn-primary"
+      @click="isModalVisible = true"
+    >
       Open modal
     </button>
-    <!-- TODO: this should go inside the modal -->
-    <div v-if="loading">loading people...</div>
-    <ul v-else>
-      <li v-for="person in people" :key="person.url">
-        {{ person.name }}
-      </li>
-    </ul>
-    <!-- end of modal -->
+    <app-modal
+      title="People"
+      v-model="isModalVisible"
+      @ok="isModalVisible = false"
+    >
+      <div v-if="loading">loading people...</div>
+      <ul v-else>
+        <li v-for="person in people" :key="person.url">
+          {{ person.name }}
+        </li>
+      </ul>
+    </app-modal>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-// import Modal from './components/Modal.vue';
+import Modal from './components/Modal.vue';
 
 export default {
   name: 'app',
-  // components: {
-  //   Modal,
-  // },
-  data() {
+  components: {
+    AppModal: Modal,
+  },
+  data () {
     return {
       loading: false,
       people: [],
+      isModalVisible: false,
     };
   },
-  created() {
+  created () {
     this.loadPeople();
   },
   methods: {
-    loadPeople() {
+    loadPeople () {
       this.loading = true;
       axios.get('https://swapi.co/api/people').then(res => {
         this.people = res.data.results;
@@ -48,8 +56,9 @@ export default {
 body,
 button {
   font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
-    'Lucida Sans', Arial, sans-serif;
+  'Lucida Sans', Arial, sans-serif;
 }
+
 button {
   font-size: 16px;
 }
