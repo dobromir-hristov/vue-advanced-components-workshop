e04e148d9e8a28b2f5322e46af153a5bb4fba73e
Dobromir Hristov <dobromir92@gmail.com>
2019-04-07 12:19:10 +0300

    feat (task3): semi working example

--- tasks/3/src/components/SelectOther.vue
+++ tasks/3/src/components/SelectOther.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
     <div>
-      <select>
-        <option v-for="option in options" :key="option" :value="option">{{
-          option
-        }}</option>
+      <select v-model="computedValue">
+        <option v-for="option in options" :key="option" :value="option">
+          {{ option }}
+        </option>
+        <option value="__other__">Other</option>
       </select>
     </div>
-    <div class="input">
-      <input />
+    <div class="input" v-if="otherVisible">
+      <input v-model="otherOption"/>
     </div>
   </div>
 </template>
@@ -16,13 +17,54 @@
 <script>
 export default {
   name: 'select-other',
+  data () {
+    return {
+      otherOption: '',
+      otherVisible: false,
+    };
+  },
   props: {
     options: {
       type: Array,
       required: true,
     },
+    value: {
+      type: String,
+      default: null,
+    },
   },
-  // TODO
+  computed: {
+    computedValue: {
+      get () {
+        if (this.options.includes(this.value) || this.value === null) {
+          return this.value;
+        }
+        return '__other__';
+      },
+      set (value) {
+        if (value === '__other__') {
+          this.otherVisible = true;
+          this.otherOption = ''
+          return false;
+        }
+        this.otherVisible = false;
+        this.$emit('input', value);
+      },
+    },
+  },
+  watch: {
+    otherOption (value) {
+      this.$emit('input', value);
+    },
+    value (val) {
+      if (val !== null && !this.options.includes(val)) {
+        this.otherOption = val;
+        this.otherVisible = true;
+      } else {
+        this.otherVisible = false;
+      }
+    },
+  },
 };
 </script>
 
@@ -31,6 +73,7 @@ select,
 input {
   width: 200px;
 }
+
 .input {
   margin-top: 10px;
 }
